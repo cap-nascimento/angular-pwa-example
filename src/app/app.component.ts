50f20a37f180f9fb1c6a858ab2c4c16603a04e3d
cap-nascimento <andersonkuro@gmail.com>
2020-12-09 14:54:05 -0300

    adding pwa module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { RestApiService } from './rest-api.service';
+
+export interface TableElement {
+  id: string;
+  name: string;
+  email: string;
+  website: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +16,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-pwa-example';
+  Data: TableElement[] = [];
+  col: string[] = ['id', 'name', 'email', 'website'];
+  dataSource = new MatTableDataSource<TableElement>(this.Data);
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+
+  constructor(private restApiService: RestApiService) {
+    this.restApiService.getUsers().subscribe((res) => {
+      this.dataSource = new MatTableDataSource<TableElement>(res);
+      setTimeout(() => {
+        this.dataSource.paginator = this.paginator;
+      }, 0);
+    })
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
